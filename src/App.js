9d9bb446a5e96eecdb3b20bd71ba81f66b460728
Nebojša Draganić <nebojsa311@gmail.com>
2021-05-14 18:46:45 +0200

    css tweak

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="sectionOne">
+        <div id="firstSquare"></div>
+        <div id="secondSquare">
+          <section>
+            <h1>Discover innovative ways to decorate</h1> <br></br>
+            <p>We provide unmatched quality,
+            comfort, and style for property owners across the country. Our
+            experts combine form and function in bringing your vision to life.
+            Create a room in your own style with our collection and make your
+            property a reflection of you and what you love.</p> <br></br>
+            <a href="#">Shop now</a>
+          </section>
+        </div>
+      </div>
+      <div id="sectionTwo">
+        <div id="thirdSquare"></div>
+        <div id="fourthSquare">
+          <section>
+            <h1>About our furniture</h1> 
+            <p>Our multifunctional collection blends design and
+            function to suit your individual taste. Make each room unique, or
+            pick a cohesive theme that best express your interests and what
+            inspires you. Find the furniture pieces you need, from traditional
+            to contemporary styles or anything in between. Product specialists
+            are available to help you create your dream space.</p>
+          </section>
+        </div>
+        <div id="fifthSquare"></div>
+      </div>
     </div>
   );
 }

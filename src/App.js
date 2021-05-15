18ec730cb575a18cb3ac6eefcfc222c0d6b73eae
Nebojša Draganić <nebojsa311@gmail.com>
2021-05-15 21:52:00 +0200

    css, all except responsivnes

--- src/App.js
+++ src/App.js
@@ -9,26 +9,37 @@ function App() {
         <div id="secondSquare">
           <section>
             <h1>Discover innovative ways to decorate</h1> <br></br>
-            <p>We provide unmatched quality,
-            comfort, and style for property owners across the country. Our
-            experts combine form and function in bringing your vision to life.
-            Create a room in your own style with our collection and make your
-            property a reflection of you and what you love.</p> <br></br>
-            <a href="#">Shop now</a>
+            <p>
+              We provide unmatched quality, comfort, and style for property
+              owners across the country. Our experts combine form and function
+              in bringing your vision to life. Create a room in your own style
+              with our collection and make your property a reflection of you and
+              what you love.
+            </p>{" "}
+            <br></br>
+            <a href="#">
+              SHOP NOW<span>&#x2192;</span>
+            </a>
           </section>
+          <div id="arrows">
+          <div id="arrowOne"> &gt; </div>
+          <div id="arrowTwo"> &lt; </div>
+          </div>
         </div>
       </div>
       <div id="sectionTwo">
         <div id="thirdSquare"></div>
         <div id="fourthSquare">
           <section>
-            <h1>About our furniture</h1> 
-            <p>Our multifunctional collection blends design and
-            function to suit your individual taste. Make each room unique, or
-            pick a cohesive theme that best express your interests and what
-            inspires you. Find the furniture pieces you need, from traditional
-            to contemporary styles or anything in between. Product specialists
-            are available to help you create your dream space.</p>
+            <h1>About our furniture</h1>
+            <p>
+              Our multifunctional collection blends design and function to suit
+              your individual taste. Make each room unique, or pick a cohesive
+              theme that best express your interests and what inspires you. Find
+              the furniture pieces you need, from traditional to contemporary
+              styles or anything in between. Product specialists are available
+              to help you create your dream space.
+            </p>
           </section>
         </div>
         <div id="fifthSquare"></div>

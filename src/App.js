ca11025c5ae46b72f5f318b9bf92700f67805e0d
unknown <nebojsa311@gmail.com>
2021-05-21 20:42:31 +0200

    css

--- src/App.js
+++ src/App.js
@@ -17,9 +17,11 @@ class App extends React.Component {
       i: 0,
       text: TEXTS,
       headers: HEADERS,
+      closed: true,
     };
     this.slider = this.slider.bind(this);
     this.sliderBack = this.sliderBack.bind(this);
+    this.hamburgerFunc = this.hamburgerFunc.bind(this);
   }
 
   slider() {
@@ -35,10 +37,35 @@ class App extends React.Component {
       this.setState({ image: images[this.state.i - 1], i: this.state.i - 1 });
     }
   }
+
+  hamburgerFunc() {
+    if(this.state.closed) {
+      let navOptions = document.getElementById("list");
+      let navWrapper = document.getElementById("navbarWrapper");
+      let hamButton = document.getElementById("hamburger");
+      let closeButton = document.getElementById("close");
+      hamButton.style.display = "none";
+      closeButton.style.display = "block";
+      navOptions.style.display = "flex";
+      navWrapper.style.backgroundColor = "white";
+      this.setState({ closed: false })
+    } else if(this.state.closed === false) {
+      let navOptions = document.getElementById("list");
+      let navWrapper = document.getElementById("navbarWrapper");
+      let hamButton = document.getElementById("hamburger");
+      let closeButton = document.getElementById("close");
+      hamButton.style.display = "block";
+      closeButton.style.display = "none";
+      navOptions.style.display = "none";
+      navWrapper.style.backgroundColor = "transparent";
+      this.setState({ closed: true })
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <Navbar />
+        <Navbar toogler={this.hamburgerFunc} />
         <SectionOne
           imageOne={this.state.image}
           HEADERS={HEADERS}

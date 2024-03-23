29d1b67bd2a434decac32f37944148e5f401237f
unknown <nebojsa311@gmail.com>
2024-03-23 20:18:27 +0100

    new collection

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './Navbar.jsx'
+import NewCollection from './NewCollection.jsx'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
   return (
       <div>
         <Navbar/>
+        <NewCollection/>
       </div>
       )
 }
---
+++ src/NewCollection.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './NewCollection.css'
+
+function NewCollection() {
+    return (
+        <div className='newCollection'>
+            <div className='leftBlock'>
+                <p>Our Summer Collection</p>
+                <h1>NewCollection</h1>
+                <h1>Converse</h1>
+                <p>Discover stylish Converse arrivals, quality comfort, and innovation for your active life<br/></p>
+                </div>
+            <div className="rightBlock"><p>This is right block</p></div>
+        </div>
+    )
+}
+
+export default NewCollection;

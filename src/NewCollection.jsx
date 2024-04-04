0e33d4362171ccadb3f0bb2a3cf77791acd2c77d
unknown <nebojsa311@gmail.com>
2024-04-04 19:34:11 +0200

    added images and styled text

--- src/NewCollection.jsx
+++ src/NewCollection.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './NewCollection.css'
+import red from './assets/red.png'
+import blue from './assets/blue.png'
+import black from './assets/black.png'
 
 function NewCollection() {
     return (
@@ -7,10 +10,14 @@ function NewCollection() {
             <div className='leftBlock'>
                 <p>Our Summer Collection</p>
                 <h1>NewCollection</h1>
-                <h1>Converse</h1>
+                <h1 className='purpleHeading'>Converse</h1>
                 <p>Discover stylish Converse arrivals, quality comfort, and innovation for your active life<br/></p>
                 </div>
-            <div className="rightBlock"><p>This is right block</p></div>
+            <div className="rightBlock">
+                <img src={red}/>
+                <img src={blue}/>
+                <img src={black}/>
+            </div>
         </div>
     )
 }
